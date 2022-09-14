af74ad8dd4a76b1f71e585f6e3e5bda09f1637c3
Namkat Cedrick Jumtock <namkatcedrickjumtock3@gmail.com>
2022-09-14 12:02:54 +0100

    setup backend but no running server

---
+++ server/lib/server.dart
@@ -0,0 +1,12 @@
+/// server
+///
+/// A Aqueduct web server.
+library server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+export 'package:aqueduct/managed_auth.dart';
+
+export 'channel.dart';
